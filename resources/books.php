c251e6804c499c5be29f219a3146fa108d7db08d
imsinu9 <imsinu9@gmail.com>
2013-07-15 00:29:33 +0530

    Adding published year matrix

--- resources/books.php
+++ resources/books.php
@@ -4,7 +4,7 @@ include_once("config.php");
 $title = $_POST['title'] ? strtoupper($_POST['title']) : '%';
 $author = $_POST['author'] ? strtoupper($_POST['author']) : '%';
 $category = $_POST['category'] ? strtoupper($_POST['category']) : '%';
-$query = "select title,author,company,published_year,in_rack,category.name as name from books join category on books.category_id = category.category_id where title like ('" . $title . "') and author like('" . $author . "') and category.name like ('" . $category . "') order by popularity";
+$query = "select book_id_no,title,author,company,published_year,in_rack,category.name as name from books join category on books.category_id = category.category_id where title like ('" . $title . "') and author like('" . $author . "') and category.name like ('" . $category . "') order by popularity desc,published_year desc,in_rack desc";
 $result = mysql_query($query);
 //echo mysql_num_rows($result);
 ?>
@@ -29,6 +29,7 @@ $result = mysql_query($query);
                                 <th>Author</th>
                                 <th>Company</th>
                                 <th>Category</th>
+                                <th>Published On</th>
                                 <th>Available</th>
                                 <th>Action</th>
                             </tr>
@@ -44,14 +45,16 @@ $result = mysql_query($query);
                                 <td>" . $row['author'] . "</td>
                                 <td>" . $row['company'] . "</td>
                                 <td>" . $row['name'] . "</td>
+                                <td>" . $row['published_year'] . "</td>
                                 <td>" . $row['in_rack'] . "</td>
-                                <td><button class='btn btn-info' type='button'>Issue
-                                </button></td>
+                                <td><a href='confirm_issue.php?id=". $row['book_id_no'] ."'><button class='btn btn-info' type='button'>Issue
+                                </button></a></td>
                             </tr>";
                             }?>
                         </tbody>
                     </table>
-
+<!--                    <a href='confirm_issue.php?id="aaa"'><button class='btn btn-info' type='button'>Issue-->
+<!--                    </button></a>-->
                 </div>
             </div>
         </div>
