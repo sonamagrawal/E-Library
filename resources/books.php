823f44e5a6c466c1611a53766cf99f5b095188d5
imsinu9 <imsinu9@gmail.com>
2013-07-14 23:05:21 +0530

    Adding book searched result

---
+++ resources/books.php
@@ -0,0 +1,59 @@
+<!DOCTYPE HTML>
+<?php
+include_once("config.php");
+$title = $_POST['title'] ? strtoupper($_POST['title']) : '%';
+$author = $_POST['author'] ? strtoupper($_POST['author']) : '%';
+$category = $_POST['category'] ? strtoupper($_POST['category']) : '%';
+$query = "select title,author,company,published_year,in_rack,category.name as name from books join category on books.category_id = category.category_id where title like ('" . $title . "') and author like('" . $author . "') and category.name like ('" . $category . "') order by popularity";
+$result = mysql_query($query);
+//echo mysql_num_rows($result);
+?>
+<html>
+    <head>
+        <title> E-LIBRARY |BOOK </title>
+        <link href="/public_html/css/bootstrap.css" type="text/css" rel="stylesheet">
+        <link href="/public_html/css/bootstrap-responsive.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="span3" style="border:1px solid black">
+                </div>
+                <div class="span9" style="border:1px solid black">
+                    <table class="table table-striped">
+                        <caption><i class="icon-tags"></i> Matched Result</caption>
+                        <thead style="background: peru">
+                            <tr>
+                                <th>#</th>
+                                <th>Title</th>
+                                <th>Author</th>
+                                <th>Company</th>
+                                <th>Category</th>
+                                <th>Available</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $counter = 0;
+                            while ($row = mysql_fetch_array($result)) {
+                                $counter++;
+                                $status = $row['in_rack'] == 0 ? 'error' : 'success' ;
+                                echo "<tr class='".$status."'>
+                                <td>" . $counter . "</td>
+                                <td>" . $row['title'] . "</td>
+                                <td>" . $row['author'] . "</td>
+                                <td>" . $row['company'] . "</td>
+                                <td>" . $row['name'] . "</td>
+                                <td>" . $row['in_rack'] . "</td>
+                                <td><button class='btn btn-info' type='button'>Issue
+                                </button></td>
+                            </tr>";
+                            }?>
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
