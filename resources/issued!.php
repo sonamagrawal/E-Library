bd20f23fafeaa95ef4772569d756ff64995aafbd
imsinu9 <imsinu9@gmail.com>
2013-07-15 23:23:00 +0530

    Added page for user profile

--- resources/issued!.php
+++ resources/issued!.php
@@ -1,18 +1,18 @@
 <?php
 include_once("config.php");
-$card_id = $_POST['card_id'] ;
-$book_id = $_POST['bookID'] ;
+$card_id = $_POST['card_id'];
+$book_id = $_POST['bookID'];
 $_SESSION['u_id'] = '1';
 
-$query = "insert into issued_books(BOOK_ID_NO,USER_ID,CARD_ID,ISSUED_DATE,RETURN_DATE) values (".$book_id.",'".$_SESSION['u_id']."','".$card_id."',now(),DATE_ADD(now(), INTERVAL 2 MONTH))";
+$query = "insert into issued_books(BOOK_ID_NO,USER_ID,CARD_ID,ISSUED_DATE,RETURN_DATE) values (" . $book_id . ",'" . $_SESSION['u_id'] . "','" . $card_id . "',now(),DATE_ADD(now(), INTERVAL 2 MONTH))";
 $result = mysql_query($query);
 
-$query = "UPDATE card_detail set ISSUED = 'Y' where CARD_ID = '".$card_id."'" ;
+$query = "UPDATE card_detail set ISSUED = 'Y' where CARD_ID = '" . $card_id . "'";
 $result_1 = mysql_query($query);
 
-$query = "UPDATE books set popularity = popularity+1, in_rack = in_rack-1 where BOOK_ID_NO = ".$book_id ;
+$query = "UPDATE books set popularity = popularity+1, in_rack = in_rack-1 where BOOK_ID_NO = " . $book_id;
 $result_2 = mysql_query($query);
-header("location:search_book.php") ;
+header("location:search_book.php");
 
 
 
--- resources/profile.php
+++ resources/profile.php
@@ -1,1 +1,63 @@
+<!DOCTYPE HTML>
 <?php
+include_once("config.php");
+$_SESSION['u_id'] = '1';
+
+$query = "select title,author,company,published_year,issued_date,return_date from books join issued_books
+            on books.book_id_no = issued_books.book_id_no where issued_books.user_id = '" . $_SESSION['u_id'] . "' and issued_books.returned = 'N'";
+$result = mysql_query($query);
+?>
+<html>
+    <head>
+        <title> E-LIBRARY |DASHBOARD </title>
+        <link href="/public_html/css/bootstrap.css" type="text/css" rel="stylesheet">
+        <link href="/public_html/css/bootstrap-responsive.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row-fluid">
+
+                <div class="span3" style="border:1px solid black">
+                    <div class="row-fluid">
+                        <div class="span12">PROFILE</div>
+                    </div>
+                </div>
+
+                <div class="span9" style="border:1px solid black">
+                    <div class="row-fluid">
+                        <div class="span12">
+                            <table class="table table-hover">
+                                <caption><i class="icon-thumbs-up"></i>BOOKS IN HAND</caption>
+                                <thead style="background: peru">
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Title</th>
+                                        <th>Author</th>
+                                        <th>Company</th>
+                                        <th>Published On</th>
+                                        <th>Issued-Date</th>
+                                        <th>Return-Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php $counter = 0;
+                                    while ($row = mysql_fetch_array($result)) {
+                                        $counter++;
+                                        echo "<tr class='success'>
+                                            <td>" . $counter . "</td>
+                                            <td>" . $row['title'] . "</td>
+                                            <td>" . $row['author'] . "</td>
+                                            <td>" . $row['company'] . "</td>
+                                            <td>" . $row['published_year'] . "</td>
+                                            <td>" . $row['issued_date'] . "</td>
+                                            <td>" . $row['return_date'] . "</td>
+                                            </tr>";
+                                    }?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </body>
+</html>
