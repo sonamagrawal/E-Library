192b5af1f4a992792b51f8c374023861d1f4d161
sonam <sonamagrawalnitrr@gmai.com>
2013-07-21 15:53:49 +0530

    modified due_clearance.php

--- resources/due_clearance.php
+++ resources/due_clearance.php
@@ -1,12 +1,18 @@
 <!DOCTYPE HTML>
 <?php
 include_once("config.php");
+
+$query = "select roll_no from student_record ";
+$result = mysql_query($query);
+$id = '';
+while ($row = mysql_fetch_array($result)) {
+    $id .= '"' . $row['roll_no'] . '",';
+}
+$id_list = '[' . rtrim($id, ",") . ']';
 ?>
-
-
 <html>
 <head>
-    <title> E-LIBRARY |DUE CLEARANCE</title>
+    <title> E-LIBRARY |DUE_CLEARANCE </title>
     <link href="/public_html/css/bootstrap.css" type="text/css" rel="stylesheet">
     <link href="/public_html/css/bootstrap-responsive.css" type="text/css" rel="stylesheet">
 </head>
@@ -19,28 +25,20 @@ include_once("config.php");
         <div class="span9 well" style="border:1px solid black">
             <div class="row-fluid">
                 <div class="span12" style="background-color: #808080">
-                    <h2><strong>DUE CLEARANCE</strong></h2>
+                    <h2><strong>DUE_CLEARANCE</strong></h2>
                 </div>
             </div>
             </br>
             <div class="row-fluid">
                 <div class="span12">
-                    <form class="form-horizontal" action="pay.php" method="POST">
-                        <div class="control-group">
-                            <label class="control-label" >USER NAME</label>
-                            <div class="controls">
-                                <input type="text"required class="input-xlarge" id="input01">
-                             </div>
-                        </div>
-
-                        <div class="control-group">
-                            <label class="control-label" >ROLL NO.</label>
-                            <div class="controls">
-                                <input type="text"required class="input-xlarge" id="input02">
-                            </div>
-                        </div>
-                        &nbsp;&nbsp;&nbsp;   &nbsp;&nbsp;&nbsp;   &nbsp;&nbsp;&nbsp;  &nbsp;&nbsp;&nbsp;
-                        &nbsp;&nbsp;&nbsp;  &nbsp;&nbsp;&nbsp;   &nbsp;&nbsp;&nbsp;   &nbsp;&nbsp;&nbsp;
+                    <form class="form-inline" action="confirm-due_clearance.php" method="POST">
+                        <input type="text" required class="span3" name="issue"
+                               placeholder="Enter roll_no"
+                               autocomplete="off"
+                               data-provide="typeahead"
+                               data-items="6"
+                               min-length="1"
+                               data-source='<?php echo $id_list ?>'>
                         <button type="submit" class="btn btn-success">&nbspGO !&nbsp
                         </button>
                     </form>
@@ -54,8 +52,3 @@ include_once("config.php");
 <script src="/public_html/jquery/jquery.js"></script>
 <script src="/public_html/js/bootstrap-typeahead.js"></script>
 </html>
-
-
-
-
-
