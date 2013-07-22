7afd113a653847ad3a10e8709cb1cefcacba8eab
imsinu9 <imsinu9@gmail.com>
2013-07-22 22:02:05 +0530

    MaKING roll number distict

--- resources/confirm-due_clearance.php
+++ resources/confirm-due_clearance.php
@@ -17,105 +17,99 @@ join fine_detail on
 fine_detail.issue_code = issued_books.issue_code
 join books on
 books.book_id_no = issued_books.book_id_no
-where roll_no = ".$roll_no." and issued_books.fine='Y'";
+where roll_no = " . $roll_no . " and issued_books.fine='Y'";
 $result1 = mysql_query($query1);
-
-
-
-
-
-
 ?>
 <html>
-<head>
-    <title> E-LIBRARY |DUE_CLEARANCE </title>
-    <link href="/public_html/css/bootstrap.css" type="text/css" rel="stylesheet">
-    <link href="/public_html/css/bootstrap-responsive.css" type="text/css" rel="stylesheet">
-</head>
-<body>
-<div class="container-fluid">
-    <div class="row-fluid">
-        <div class="span3" style="border:1px solid black">
-        </div>
-        <div class="span9 well" style="border:1px solid black;font-size: 16px">
-            <div class="row-fluid">
-                <div class="span12" style="background-color: #808080">
-                    <h2><strong>CONFIRM DUE CLEARANCE</strong></h2>
-                </div>
-            </div>
-            </br>
-            <form action="due_clearance!.php" method="POST">
-                <div class="row-fluid">
-                    <div class="span4">
-                        <b>username : </b> <?php echo $row['first_name'] . " " . $row['last_name'] ?>
-                    </div>
-                    <div class="span4">
-                        <b>roll_no. : </b> <?php echo $row['roll_no'] ?>
-                    </div>
-                </div>
-                <div class="row-fluid">
-                    <div class="span4">
-                        <b>enrollment_no.: </b> <?php echo $row['enrollment_no'] ?>
-                    </div>
-                    <div class="span4">
-                        <b>branch : </b> <?php echo $row['branch'] ?>
-                    </div>
-                </div>
-                <div class="row-fluid">
-                    <div class="span4">
-                        <b>semester : </b> <?php echo $row['semester'] ?>
-                    </div>
+	<head>
+		<title> E-LIBRARY |DUE_CLEARANCE </title>
+		<link href="/public_html/css/bootstrap.css" type="text/css" rel="stylesheet">
+		<link href="/public_html/css/bootstrap-responsive.css" type="text/css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="container-fluid">
+			<div class="row-fluid">
+				<div class="span3" style="border:1px solid black">
+				</div>
+				<div class="span9 well" style="border:1px solid black;font-size: 16px">
+					<div class="row-fluid">
+						<div class="span12" style="background-color: #808080">
+							<h2><strong>CONFIRM DUE CLEARANCE</strong></h2>
+						</div>
+					</div>
+					</br>
+					<form action="due_clearance!.php" method="POST">
+						<div class="row-fluid">
+							<div class="span4">
+								<b>username : </b> <?php echo $row['first_name'] . " " . $row['last_name'] ?>
+							</div>
+							<div class="span4">
+								<b>roll_no. : </b> <?php echo $row['roll_no'] ?>
+							</div>
+						</div>
+						<div class="row-fluid">
+							<div class="span4">
+								<b>enrollment_no.: </b> <?php echo $row['enrollment_no'] ?>
+							</div>
+							<div class="span4">
+								<b>branch : </b> <?php echo $row['branch'] ?>
+							</div>
+						</div>
+						<div class="row-fluid">
+							<div class="span4">
+								<b>semester : </b> <?php echo $row['semester'] ?>
+							</div>
 
 
-                </div>
-                </br>
-                <hr>
-                <div class="row-fluid">
-                    <div class="span2">
-                        <button id="comfirm" type="submit" class="btn btn-large btn-success"
-                                name="confirm"><i class="icon-ok"></i>CONFIRM
-                        </button>
-                    </div>
-                    <div class="span2">
-                        <button type="button" class="btn btn-large btn-warning" name="cancel"
-                                onclick="javascript:history.go(-1)">
-                            <i class="icon-remove"></i>CANCEL
-                        </button>
-                    </div>
+						</div>
+						</br>
+						<hr>
+						<div class="row-fluid">
+							<div class="span2">
+								<button id="comfirm" type="submit" class="btn btn-large btn-success"
+								        name="confirm"><i class="icon-ok"></i>CONFIRM
+								</button>
+							</div>
+							<div class="span2">
+								<button type="button" class="btn btn-large btn-warning" name="cancel"
+								        onclick="javascript:history.go(-1)">
+									<i class="icon-remove"></i>CANCEL
+								</button>
+							</div>
 
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+		</div>
 
-<div class="container-fluid">
-    <div class="row-fluid">
-        <div class="span3" style="border:1px solid black">
-        </div>
-        <div class="span9" style="border:1px solid black">
-            <table class="table table-striped">
-                <caption><i class="icon-tags"></i> Matched Result</caption>
-                <thead style="background: peru">
-                <tr>
-                    <th>#</th>
-                    <th>Issue Code</th>
-                    <th>Title</th>
-                    <th>Author</th>
-                    <th>Fine</th>
+		<div class="container-fluid">
+			<div class="row-fluid">
+				<div class="span3" style="border:1px solid black">
+				</div>
+				<div class="span9" style="border:1px solid black">
+					<table class="table table-striped">
+						<caption><i class="icon-tags"></i> Matched Result</caption>
+						<thead style="background: peru">
+							<tr>
+								<th>#</th>
+								<th>Issue Code</th>
+								<th>Title</th>
+								<th>Author</th>
+								<th>Fine</th>
 
 
-                    <th>Action</th>
-                </tr>
-                </thead>
-                <tbody>
+								<th>Action</th>
+							</tr>
+						</thead>
+						<tbody>
 
-                <?php $counter = 0;
-                while ($row = mysql_fetch_array($result1)) {
-                    $counter++;
+							<?php $counter = 0;
+							while ($row = mysql_fetch_array($result1)) {
+								$counter++;
 
-                    echo "<tr>
+								echo "<tr>
                                 <td>" . $counter . "</td>
 
 
@@ -132,11 +126,11 @@ $result1 = mysql_query($query1);
                                 <button class='btn btn-info' type='button'>pay
                                 </button></td>
                             </tr>";
-                }?>
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
-</body>
+							}?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</body>
 </html>
--- resources/due_clearance.php
+++ resources/due_clearance.php
@@ -2,55 +2,54 @@
 <?php
 include_once("config.php");
 
-$query = "select roll_no from student_record ";
+$query = "select distinct roll_no from student_record ";
 $result = mysql_query($query);
-
 
 $id = '';
 while ($row = mysql_fetch_array($result)) {
-    $id .= '"' . $row['roll_no'] . '",';
+	$id .= '"' . $row['roll_no'] . '",';
 }
 $id_list = '[' . rtrim($id, ",") . ']';
 ?>
 <html>
-<head>
-    <title> E-LIBRARY |DUE_CLEARANCE </title>
-    <link href="/public_html/css/bootstrap.css" type="text/css" rel="stylesheet">
-    <link href="/public_html/css/bootstrap-responsive.css" type="text/css" rel="stylesheet">
-</head>
-<body>
-<div class="container-fluid">
-    <div class="row-fluid">
-        <div class="span3" style="border:1px solid black">
-        </div>
+	<head>
+		<title> E-LIBRARY |DUE_CLEARANCE </title>
+		<link href="/public_html/css/bootstrap.css" type="text/css" rel="stylesheet">
+		<link href="/public_html/css/bootstrap-responsive.css" type="text/css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="container-fluid">
+			<div class="row-fluid">
+				<div class="span3" style="border:1px solid black">
+				</div>
 
-        <div class="span9 well" style="border:1px solid black">
-            <div class="row-fluid">
-                <div class="span12" style="background-color: #808080">
-                    <h2><strong>DUE_CLEARANCE</strong></h2>
-                </div>
-            </div>
-            </br>
-            <div class="row-fluid">
-                <div class="span12">
-                    <form class="form-inline" action="confirm-due_clearance.php" method="POST">
-                        <input type="text" required class="span3" name="rollno"
-                               placeholder="Enter roll_no"
-                               autocomplete="off"
-                               data-provide="typeahead"
-                               data-items="6"
-                               min-length="1"
-                               data-source='<?php echo $id_list ?>'>
-                        <button type="submit" class="btn btn-success">&nbspGO !&nbsp
-                        </button>
-                    </form>
-                </div>
-            </div>
-            </br></br></br></br></br></br>
-        </div>
-    </div>
-</div>
-</body>
-<script src="/public_html/jquery/jquery.js"></script>
-<script src="/public_html/js/bootstrap-typeahead.js"></script>
+				<div class="span9 well" style="border:1px solid black">
+					<div class="row-fluid">
+						<div class="span12" style="background-color: #808080">
+							<h2><strong>DUE_CLEARANCE</strong></h2>
+						</div>
+					</div>
+					</br>
+					<div class="row-fluid">
+						<div class="span12">
+							<form class="form-inline" action="confirm-due_clearance.php" method="POST">
+								<input type="text" required class="span3" name="rollno"
+								       placeholder="Enter roll_no"
+								       autocomplete="off"
+								       data-provide="typeahead"
+								       data-items="6"
+								       min-length="1"
+								       data-source='<?php echo $id_list ?>'>
+								<button type="submit" class="btn btn-success">&nbspGO !&nbsp
+								</button>
+							</form>
+						</div>
+					</div>
+					</br></br></br></br></br></br>
+				</div>
+			</div>
+		</div>
+	</body>
+	<script src="/public_html/jquery/jquery.js"></script>
+	<script src="/public_html/js/bootstrap-typeahead.js"></script>
 </html>
