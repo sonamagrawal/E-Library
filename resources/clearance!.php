dadd3dd0d791b5b8572bf00d708d13fa1100f322
imsinu9 <imsinu9@gmail.com>
2013-07-22 23:31:11 +0530

    Adding function to update columns post payment

---
+++ resources/clearance!.php
@@ -0,0 +1,9 @@
+<?php
+include_once("config.php");
+$roll_no = $_POST['roll_no'] ;
+$query = "Update student_record join issued_books on issued_books.user_id = student_record.user_id
+		  join fine_detail on fine_detail.issue_code = issued_books.issue_code
+		  set fine_detail.paid = 'Y'
+		  where student_record.roll_no = '" . $roll_no . "' and issued_books.fine= 'Y'" ;
+$result = mysql_query($query);
+header("location:profile.php");
--- resources/confirm-due_clearance.php
+++ resources/confirm-due_clearance.php
@@ -3,26 +3,25 @@
 include_once("config.php");
 $roll_no = $_POST['rollno'];
 
-$query = "select first_name,last_name,enrollment_no,roll_no,branch,semester from student_record";
+$query = "select first_name,last_name,enrollment_no,roll_no,branch,semester from student_record 
+		  where roll_no = '" . $roll_no . "'";
+
 $result = mysql_query($query);
+if (mysql_num_rows($result) == 0) {
+	header("location:javascript://:history.go(-1)");
+}
 $row = mysql_fetch_array($result);
 
-
-$query1 = "select issued_books.issue_code,
-title,author,fine_detail.fine
-from student_record
-join issued_books on
-issued_books.user_id = student_record.user_id
-join fine_detail on
-fine_detail.issue_code = issued_books.issue_code
-join books on
-books.book_id_no = issued_books.book_id_no
-where roll_no = " . $roll_no . " and issued_books.fine='Y'";
+$query1 = "select issued_books.issue_code,title,author,fine_detail.fine from student_record
+		   join issued_books on issued_books.user_id = student_record.user_id join fine_detail
+		   on fine_detail.issue_code = issued_books.issue_code join books on
+		   books.book_id_no = issued_books.book_id_no where roll_no = '" . $roll_no . "' and 
+		   issued_books.fine='Y' and fine_detail.paid = 'N' ";
 $result1 = mysql_query($query1);
 ?>
 <html>
 	<head>
-		<title> E-LIBRARY |DUE CLEARANCE </title>
+		<title> E-LIBRARY |CONFIRM CLEARANCE </title>
 		<link href="/public_html/css/bootstrap.css" type="text/css" rel="stylesheet">
 		<link href="/public_html/css/bootstrap-responsive.css" type="text/css" rel="stylesheet">
 	</head>
@@ -38,97 +37,73 @@ $result1 = mysql_query($query1);
 						</div>
 					</div>
 					</br>
-					<form action="due_clearance!.php" method="POST">
 						<div class="row-fluid">
 							<div class="span4">
-								<b>username : </b> <?php echo $row['first_name'] . " " . $row['last_name'] ?>
+								<b>Username : </b> <?php echo $row['first_name'] . " " . $row['last_name'] ?>
 							</div>
 							<div class="span4">
-								<b>roll_no. : </b> <?php echo $row['roll_no'] ?>
+								<b>Roll No. : </b> <?php echo $row['roll_no'] ?>
 							</div>
 						</div>
 						<div class="row-fluid">
 							<div class="span4">
-								<b>enrollment_no.: </b> <?php echo $row['enrollment_no'] ?>
+								<b>Enrollment No. : </b> <?php echo $row['enrollment_no'] ?>
 							</div>
 							<div class="span4">
-								<b>branch : </b> <?php echo $row['branch'] ?>
+								<b>Branch : </b> <?php echo $row['branch'] ?>
 							</div>
 						</div>
 						<div class="row-fluid">
 							<div class="span4">
-								<b>semester : </b> <?php echo $row['semester'] ?>
+								<b>Semester : </b> <?php echo $row['semester'] ?>
 							</div>
-
-
 						</div>
-						</br>
 						<hr>
 						<div class="row-fluid">
+							<div class="span12">
+								<table class="table table-striped">
+									<thead style="background: peru">
+										<tr>
+											<th>#</th>
+											<th>Issue Code</th>
+											<th>Title</th>
+											<th>Author</th>
+											<th>Fine</th>
+										</tr>
+									</thead>
+									<tbody>
+										<?php $counter = 0;
+										while ($row1 = mysql_fetch_array($result1)) {
+											$counter++;
+											echo "<tr>
+					                                <td>" . $counter . "</td>
+					                                <td>" . $row1['issue_code'] . "</td>
+					                                <td>" . $row1['title'] . "</td>
+					                                <td>" . $row1['author'] . "</td>
+					                                <td>" . $row1['fine'] . "</td>
+					                            </tr>";
+										}?>
+									</tbody>
+								</table>
+							</div>
+						</div>
+						<hr>
+					<form action="clearance!.php" method="POST">
+						<input type="hidden" name="roll_no" value='<?php echo $roll_no ?>'>
+						<div class="row-fluid">
 							<div class="span2">
-								<button id="comfirm" type="submit" class="btn btn-large btn-success"
-								        name="confirm"><i class="icon-ok"></i>CONFIRM
+								<button type="submit" id="comfirm" class="btn btn-large btn-success"
+								        name="confirm"><i class="icon-ok"></i> Pay&nbsp&nbsp&nbsp
 								</button>
 							</div>
 							<div class="span2">
 								<button type="button" class="btn btn-large btn-warning" name="cancel"
 								        onclick="javascript:history.go(-1)">
-									<i class="icon-remove"></i>CANCEL
+									<i class="icon-remove"></i> Cancel
 								</button>
 							</div>
-
 						</div>
 					</form>
-				</div>
-			</div>
-		</div>
-		</div>
-
-		<div class="container-fluid">
-			<div class="row-fluid">
-				<div class="span3" style="border:1px solid black">
-				</div>
-				<div class="span9" style="border:1px solid black">
-					<table class="table table-striped">
-						<caption><i class="icon-tags"></i> Matched Result</caption>
-						<thead style="background: peru">
-							<tr>
-								<th>#</th>
-								<th>Issue Code</th>
-								<th>Title</th>
-								<th>Author</th>
-								<th>Fine</th>
-
-
-								<th>Action</th>
-							</tr>
-						</thead>
-						<tbody>
-
-							<?php $counter = 0;
-							while ($row = mysql_fetch_array($result1)) {
-								$counter++;
-
-								echo "<tr>
-                                <td>" . $counter . "</td>
-
-
-
-
-                                <td>" . $row['issue_code'] . "</td>
-                                <td>" . $row['title'] . "</td>
-                                <td>" . $row['author'] . "</td>
-                                <td>" . $row['fine'] . "</td>
-
-                                <td>
-
-
-                                <button class='btn btn-info' type='button'>pay
-                                </button></td>
-                            </tr>";
-							}?>
-						</tbody>
-					</table>
 				</div>
 			</div>
 		</div>
