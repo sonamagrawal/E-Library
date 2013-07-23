06628ce0f034e7876d3542ec1abdc99ad539c7c7
sonam <sonamagrawalnitrr@gmai.com>
2013-07-24 01:09:44 +0530

    added placeholder on add_books.php modified placeholder on add_books.php

--- resources/add_books.php
+++ resources/add_books.php
@@ -13,89 +13,102 @@ $id_list = '[' . rtrim($id, ",") . ']';
 ?>
 
 <html lang="en">
-	<head>
-		<link href="/public_html/css/bootstrap.css" rel="stylesheet">
-        <link href="/public_html/css/bootstrap-responsive.css" type="text/css" rel="stylesheet">
-	</head>
-	<body>
-    <div class="container-fluid">
-        <div class="row-fluid">
-            <div class="span3" style="border:1px solid black">
-            </div>
+<head>
+    <link href="/public_html/css/bootstrap.css" rel="stylesheet">
+    <link href="/public_html/css/bootstrap-responsive.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row-fluid">
+        <div class="span3" style="border:1px solid black">
+        </div>
 
-            <div class="span9 " style="border:1px solid black">
-                <div class="row-fluid">
-                    <div class="span12" style="background-color: #808080">
-                        <h2><strong>ADD BOOKS</strong></h2>
-                    </div>
+        <div class="span9 " style="border:1px solid black">
+            <div class="row-fluid">
+                <div class="span12" style="background-color: #808080">
+                    <h2><strong>ADD BOOKS</strong></h2>
                 </div>
-                <br>
-                <div class="row-fluid">
-                    <div class="span12">
-		<form action="book!.php" method="post" class=" form-horizontal">
-			<fieldset>
+            </div>
+            <br>
 
-				<div class="control-group">
-					<label class="control-label">TITLE</label>
+            <div class="row-fluid">
+                <div class="span12">
+                    <form action="book!.php" method="post" class=" form-horizontal">
+                        <fieldset>
 
-					<div class="controls">
-						<input type="text" required class="span3" name="title" placeholder="title">
-					</div>
-				</div>
-				<div class="control-group">
-					<label class="control-label">AUTHOR</label>
+                            <div class="control-group">
+                                <label class="control-label">TITLE</label>
 
-					<div class="controls">
+                                <div class="controls">
+                                    <input type="text" required class="span3" name="title" placeholder="eg:abc,xyz">
+                                </div>
+                            </div>
+                            <div class="control-group">
+                                <label class="control-label">AUTHOR</label>
 
-						<input type="text" required class="span3" name="author" placeholder="eg:abc,xyz">
-					</div>
-				</div>
+                                <div class="controls">
 
-				<div class="control-group">
-					<label class="control-label">COMPANY</label>
+                                    <input type="text" required class="span3" name="author" placeholder="eg:abc,xyz">
+                                </div>
+                            </div>
 
-					<div class="controls">
+                            <div class="control-group">
+                                <label class="control-label">COMPANY</label>
 
-						<input type="text" required class="span3" name="company" placeholder="company">
-					</div>
-				</div>
+                                <div class="controls">
 
-				<div class="control-group">
-					<label class="control-label">PUBLISHED YEAR</label>
+                                    <input type="text" required class="span3" name="company" placeholder="company">
+                                </div>
+                            </div>
 
-					<div class="controls">
-						<input type="text" required class="span3" name="year" placeholder="YYYY">
-					</div>
-				</div>
+                            <div class="control-group">
+                                <label class="control-label">PUBLISHED YEAR</label>
 
-				<div class="control-group">
-					<label class="control-label">NO. OF COPIES</label>
+                                <div class="controls">
+                                    <select required name="year">
 
-					<div class="controls">
-						<input required type="text" class="span3" name="copies" placeholder="copies">
-					</div>
-				</div>
+                                        <?php
+
+                                        for ($i = 1980; $i < 2014; $i++) {
+                                            echo "<option>$i </option>";
+                                        }
+
+                                        ?>
+
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="control-group">
+                                <label class="control-label">NO. OF COPIES</label>
+
+                                <div class="controls">
+                                    <input required type="number" class="span3" name="copies" min="1" max="50"
+                                           placeholder="1">
+                                </div>
+                            </div>
 
 
-				<div class="control-group">
-					<label class="control-label">CATEGORY</label>
+                            <div class="control-group">
+                                <label class="control-label">CATEGORY</label>
 
-					<div class="controls">
-						<input required type="text" class="span3" name="category" placeholder="eg:mathematics"
-                               autocomplete="off"
-                               data-provide="typeahead"
-                               data-items="6"
-                               min-length="1"
-                               data-source='<?php echo $id_list ?>'>
-					</div>
-				</div>
-				<div class="form-actions">
-					<button type="submit" class="btn btn-success" name="submit">ADD</button>
-					<button type="reset" class="btn btn-success" name="reset">RESET</button>
-				</div>
-			</fieldset>
-		</form>
-	</body>
-    <script src="/public_html/jquery/jquery.js"></script>
-    <script src="/public_html/js/bootstrap-typeahead.js"></script>
+                                <div class="controls">
+                                    <input required type="text" class="span3" name="category"
+                                           placeholder="eg:mathematics"
+                                           autocomplete="off"
+                                           data-provide="typeahead"
+                                           data-items="6"
+                                           min-length="1"
+                                           data-source='<?php echo $id_list ?>'>
+                                </div>
+                            </div>
+                            <div class="form-actions">
+                                <button type="submit" class="btn btn-success" name="submit">ADD</button>
+                                <button type="reset" class="btn btn-success" name="reset">RESET</button>
+                            </div>
+                        </fieldset>
+                    </form>
+</body>
+<script src="/public_html/jquery/jquery.js"></script>
+<script src="/public_html/js/bootstrap-typeahead.js"></script>
 </html>
