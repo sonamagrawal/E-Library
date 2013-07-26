cf66fff2992431083daf3311ac5b1974e47863c0
imsinu9 <imsinu9@gmail.com>
2013-07-27 01:32:28 +0530

    Adding feature to issue student library cards when new student added

--- resources/student_record!.php
+++ resources/student_record!.php
@@ -9,9 +9,14 @@ $semester = strtoupper(trim($_POST['semester']));
 
 $query = "insert into student_record (roll_no,first_name,last_name,enrollment_no,branch,semester)values
         ('" . $roll_no . "','" . $first_name . "','" . $last_name . "','" . $enrollment_no . "','" .
-	$branch . "'," . $semester . ")";
+	     $branch . "'," . $semester . ")";
 mysql_query($query);
 
 $insert_login = "insert into login(user_name, password) values('" . $roll_no . "',md5('" . $enrollment_no . "'))";
 mysql_query($insert_login);
+$user_id = mysql_insert_id() ;
+
+$insert_card_id = "insert into card_detail(user_id) values (".$user_id."), (".$user_id.")" ;
+mysql_query($insert_card_id);
+
 header("location:student_record.php");
