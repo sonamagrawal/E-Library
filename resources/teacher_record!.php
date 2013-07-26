8b847226783bd9984a7e68d5c798d99cc0e1c287
imsinu9 <imsinu9@gmail.com>
2013-07-27 01:40:36 +0530

    Issuing card id to teachers

--- resources/teacher_record!.php
+++ resources/teacher_record!.php
@@ -6,12 +6,17 @@ $last_name = strtoupper(trim($_POST['lastname']));
 $department = strtoupper(trim($_POST['department']));
 $designation = strtoupper(trim($_POST['designation']));
 
-
 $query = "insert into teacher_record (employee_no,first_name,last_name,department,designation)values
          ('" . $employee_no . "','" . $first_name . "','" . $last_name . "','" . $department . "','" .
-	$designation . "')";
+		 $designation . "')";
 mysql_query("$query");
 
-$insert_login = "insert into login(user_name, password) values('" . $employee_no . "',md5('" . $employee_no . "'))";
+$insert_login = "insert into login(user_name, password) values('" . $employee_no . "',
+				 md5('" . $employee_no . "'))";
 mysql_query($insert_login);
+$user_id = mysql_insert_id() ;
+
+$insert_card_id = "insert into card_detail(user_id) values(".$user_id."),(".$user_id."),(".$user_id.")" ;
+mysql_query($insert_card_id);
+
 header("location:teacher_record.php");
