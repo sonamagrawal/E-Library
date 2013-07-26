c49c09223e83049c8fc23e1f6b428b50ea51a2c3
imsinu9 <imsinu9@gmail.com>
2013-07-27 05:26:36 +0530

    adding background

--- resources/index.php
+++ resources/index.php
@@ -1,103 +1,149 @@
-<<<<<<< HEAD
 <!DOCTYPE HTML>
 <html lang="en">
-<head>
-    <title> E-LIBRARY |HOME </title>
-    <link href="/public_html/css/bootstrap.css" type="text/css" rel="stylesheet">
-    <link href="/public_html/css/bootstrap-responsive.css" type="text/css" rel="stylesheet">
-</head>
-<body>
-<div class="container-fluid well">
-    <div class="row-fluid">
-        <div class="span12">
-            <img src="/public_html/img/nitrrnew_03.png" width=100%>
-        </div>
-    </div>
-</div>
+	<head>
+		<title> E-LIBRARY |HOME </title>
+		<link href="/public_html/css/bootstrap.css" type="text/css" rel="stylesheet">
+		<link href="/public_html/css/bootstrap-responsive.css" type="text/css" rel="stylesheet">
+		<link href="/public_html/css/home-style.css" type="text/css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="container-fluid well">
+			<div class="row-fluid">
+				<div class="span12" style="border:1px solid black;">
+					<div class="span3" style="background-color: darkblue">
+						<br>
+						<br>
+						<br>
+						<br>
+					</div>
+					<div class="span5" style="background-color: darkblue">
+						<br>
+						<br>
+						<br>
+						<br>
+					</div>
+					<div class="span4" style="background-color: darkblue">
+						<br>
+						<br>
+						<br>
+						<br>
+					</div>
+				</div>
+			</div>
+		</div>
 
-<div class="container-fluid well">
-    <div class="row-fluid">
-        <div class="span12">
-            <div class="span3">
-                <img src="/public_html/img/e_library_logo.gif" class="img-polaroid">
-            </div>
-            <div class="span5">
+		<div class="container-fluid well">
+			<div class="row-fluid">
+				<div class="span12" style="border:1px solid black;">
+					<div class="span3" style="background-color: cornflowerblue">
+						<br>
+						<br>
+					</div>
+					<div class="span5" style="background-color: cornflowerblue">
+						<br>
+						<br>
+					</div>
+					<div class="span4" style="background-color: cornflowerblue">
+						<br>
+						<br>
+					</div>
+				</div>
+			</div>
+		</div>
 
+		<div class="container-fluid">
+			<div class="row-fluid">
+				<div class="span12" style="border:1px solid black;">
+					<div class="span8 well" style="background-color: violet">
+						<div class="span1">
+						</div>
+						<div class="span5 book-view-left" style="background-color: blue">
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+						</div>
+						<div class="span5 book-view-right" style="background-color: dimgrey">
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+						</div>
+						<div class="span1">
+						</div>
+					</div>
+					<div class="span4 well" style="background-color: gray">
+						<br>
+						<br>
+						<br>
+						<br>
+						<br>
+						<br>
+						<br>
+						<br>
+						<br>
+						<br>
+						<br>
+						<br>
+						<br>
+					</div>
+				</div>
+			</div>
+		</div>
+		<br>
+		<br>
 
-                <ul class="nav nav-tabs">
-                    <li class="active">
-                        <a href="#">Home</a></li>
-
-                    <li><a href="#">Team </a></li>
-
-                    <li><a href="#">Contact Us</a></li>
-                </ul>
-
-
-            </div>
-            <div class="span4">
-                <div class="span6">
-                    <img src="/public_html/img/user.jpg" class="img-rounded">
-                </div>
-                <div class="span6">
-                    <a class="btn btn-primary" href="#"><i class="icon-user icon-white"></i> User</a>
-                </div>
-
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="container-fluid">
-    <div class="row-fluid">
-        <div class="span12" style="border:1px solid black;">
-            <div class="span8 well" style="background-color: gray">
-
-            </div>
-            <div class="span4 well" style="background-color: gray">
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-            </div>
-        </div>
-    </div>
-</div>
-<br>
-<br>
-
-<div class="container-fluid">
-    <div class="row-fluid">
-        <div class="span12" style="border:1px solid black;">
-            <div class="span6" style="background-color: graytext">
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-            </div>
-            <div class="span6" style="background-color: graytext">
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-            </div>
-        </div>
-    </div>
-</div>
-</body>
+		<div class="container-fluid">
+			<div class="row-fluid">
+				<div class="span12" style="border:1px solid black;">
+					<div class="span6" style="background-color: graytext">
+						<br>
+						<br>
+						<br>
+						<br>
+						<br>
+						<br>
+					</div>
+					<div class="span6" style="background-color: graytext">
+						<br>
+						<br>
+						<br>
+						<br>
+						<br>
+						<br>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
 </html>
-=======
->>>>>>> content of layout deleted
