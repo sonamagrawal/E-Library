98aee0e4fa67d5bb24399be5e122297521445238
imsinu9 <imsinu9@gmail.com>
2013-07-26 22:38:31 +0530

    Adding functionality to insert teacher login credantials into login table

--- resources/teacher_record!.php
+++ resources/teacher_record!.php
@@ -4,12 +4,14 @@ $employee_no = strtoupper(trim($_POST['employeeno']));
 $first_name = strtoupper(trim($_POST['firstname']));
 $last_name = strtoupper(trim($_POST['lastname']));
 $department = strtoupper(trim($_POST['department']));
-
 $designation = strtoupper(trim($_POST['designation']));
 
 
 $query = "insert into teacher_record (employee_no,first_name,last_name,department,designation)values
-         ('" . $employee_no . "','" . $first_name . "','" . $last_name . "','" . $department . "','" . $designation . "'
-         )";
-$result = mysql_query("$query");
+         ('" . $employee_no . "','" . $first_name . "','" . $last_name . "','" . $department . "','" .
+	$designation . "')";
+mysql_query("$query");
+
+$insert_login = "insert into login(user_name, password) values('" . $employee_no . "',md5('" . $employee_no . "'))";
+mysql_query($insert_login);
 header("location:teacher_record.php");
