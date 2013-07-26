13285f1d3a2d9a8518c79f020a41590dbbf579ae
imsinu9 <imsinu9@gmail.com>
2013-07-26 21:44:04 +0530

    Fixed a small typo in semester

--- resources/student_record!.php
+++ resources/student_record!.php
@@ -4,12 +4,11 @@ $roll_no = strtoupper(trim($_POST['rollno']));
 $first_name = strtoupper(trim($_POST['firstname']));
 $last_name = strtoupper(trim($_POST['lastname']));
 $enrollment_no = strtoupper(trim($_POST['enrollmentno']));
-
 $branch = strtoupper(trim($_POST['branch']));
 $semester = strtoupper(trim($_POST['semester']));
 
 $query="insert into student_record (roll_no,first_name,last_name,enrollment_no,branch,semester)values
-         ('" . $roll_no . "','" . $first_name . "','" . $last_name . "','" . $enrollment_no . "','" . $branch . "'
-         ,'" . semester. "')";
+        ('" . $roll_no . "','" . $first_name . "','" . $last_name . "','" . $enrollment_no . "','" .
+	    $branch . "'," . $semester. ")";
 $result=mysql_query("$query");
 header("location:student_record.php");
