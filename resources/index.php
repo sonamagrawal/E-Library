f5fbf128e1b9a8512de95a293cc1d8bf7d9fd1d1
imsinu9 <imsinu9@gmail.com>
2013-07-27 05:26:36 +0530

    adding background

--- resources/index.php
+++ resources/index.php
@@ -4,6 +4,7 @@
 		<title> E-LIBRARY |HOME </title>
 		<link href="/public_html/css/bootstrap.css" type="text/css" rel="stylesheet">
 		<link href="/public_html/css/bootstrap-responsive.css" type="text/css" rel="stylesheet">
+		<link href="/public_html/css/home-style.css" type="text/css" rel="stylesheet">
 	</head>
 	<body>
 		<div class="container-fluid well">
@@ -53,20 +54,53 @@
 		<div class="container-fluid">
 			<div class="row-fluid">
 				<div class="span12" style="border:1px solid black;">
-					<div class="span8 well" style="background-color: gray">
-						<br>
-						<br>
-						<br>
-						<br>
-						<br>
-						<br>
-						<br>
-						<br>
-						<br>
-						<br>
-						<br>
-						<br>
-						<br>
+					<div class="span8 well" style="background-color: violet">
+						<div class="span1">
+						</div>
+						<div class="span5 book-view-left" style="background-color: blue">
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+						</div>
+						<div class="span5 book-view-right" style="background-color: dimgrey">
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+						</div>
+						<div class="span1">
+						</div>
 					</div>
 					<div class="span4 well" style="background-color: gray">
 						<br>
