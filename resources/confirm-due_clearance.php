dbeea805b445ef2e58bf1531cd0429eace6db4c9
imsinu9 <imsinu9@gmail.com>
2013-07-26 21:24:51 +0530

    fixed the bug . Now able to get details of fine at due clearance

--- resources/confirm-due_clearance.php
+++ resources/confirm-due_clearance.php
@@ -3,19 +3,19 @@
 include_once("config.php");
 $roll_no = $_POST['rollno'];
 
-$query = "select first_name,last_name,enrollment_no,roll_no,branch,semester from student_record 
-		  where roll_no = '" . $roll_no . "'";
+$query = "select user_id,first_name,last_name,enrollment_no,roll_no,branch,semester from student_record
+		  join login on student_record.roll_no = login.user_name where roll_no = '" . $roll_no . "'";
 
 $result = mysql_query($query);
 if (mysql_num_rows($result) == 0) {
 	header("location:javascript://:history.go(-1)");
 }
 $row = mysql_fetch_array($result);
+$user_id = $row['user_id'];
 
-$query1 = "select issued_books.issue_code,title,author,fine_detail.fine from student_record
-		   join issued_books on issued_books.user_id = student_record.user_id join fine_detail
-		   on fine_detail.issue_code = issued_books.issue_code join books on
-		   books.book_id_no = issued_books.book_id_no where roll_no = '" . $roll_no . "' and 
+$query1 = "select issued_books.issue_code,title,author,fine_detail.fine from issued_books join
+		   fine_detail on fine_detail.issue_code = issued_books.issue_code join books on
+		   books.book_id_no = issued_books.book_id_no where issued_books.user_id = " . $user_id . " and
 		   issued_books.fine='Y' and fine_detail.paid = 'N' ";
 $result1 = mysql_query($query1);
 ?>
@@ -37,63 +37,64 @@ $result1 = mysql_query($query1);
 						</div>
 					</div>
 					</br>
-						<div class="row-fluid">
-							<div class="span4">
-								<b>Username : </b> <?php echo $row['first_name'] . " " . $row['last_name'] ?>
-							</div>
-							<div class="span4">
-								<b>Roll No. : </b> <?php echo $row['roll_no'] ?>
-							</div>
+					<div class="row-fluid">
+						<div class="span4">
+							<b>Username : </b> <?php echo $row['first_name'] . " " . $row['last_name'] ?>
 						</div>
-						<div class="row-fluid">
-							<div class="span4">
-								<b>Enrollment No. : </b> <?php echo $row['enrollment_no'] ?>
-							</div>
-							<div class="span4">
-								<b>Branch : </b> <?php echo $row['branch'] ?>
-							</div>
+						<div class="span4">
+							<b>Roll No. : </b> <?php echo $row['roll_no'] ?>
 						</div>
-						<div class="row-fluid">
-							<div class="span4">
-								<b>Semester : </b> <?php echo $row['semester'] ?>
-							</div>
+					</div>
+					<div class="row-fluid">
+						<div class="span4">
+							<b>Enrollment No. : </b> <?php echo $row['enrollment_no'] ?>
 						</div>
-						<hr>
-						<div class="row-fluid">
-							<div class="span12">
-								<table class="table table-striped">
-									<thead style="background: peru">
-										<tr>
-											<th>#</th>
-											<th>Issue Code</th>
-											<th>Title</th>
-											<th>Author</th>
-											<th>Fine</th>
-										</tr>
-									</thead>
-									<tbody>
-										<?php $counter = 0;
-										while ($row1 = mysql_fetch_array($result1)) {
-											$counter++;
-											echo "<tr>
+						<div class="span4">
+							<b>Branch : </b> <?php echo $row['branch'] ?>
+						</div>
+					</div>
+					<div class="row-fluid">
+						<div class="span4">
+							<b>Semester : </b> <?php echo $row['semester'] ?>
+						</div>
+					</div>
+					<hr>
+					<div class="row-fluid">
+						<div class="span12">
+							<table class="table table-striped">
+								<thead style="background: peru">
+									<tr>
+										<th>#</th>
+										<th>Issue Code</th>
+										<th>Title</th>
+										<th>Author</th>
+										<th>Fine</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php $counter = 0;
+									while ($row1 = mysql_fetch_array($result1)) {
+										$counter++;
+										echo "<tr>
 					                                <td>" . $counter . "</td>
 					                                <td>" . $row1['issue_code'] . "</td>
 					                                <td>" . $row1['title'] . "</td>
 					                                <td>" . $row1['author'] . "</td>
 					                                <td>" . $row1['fine'] . "</td>
 					                            </tr>";
-										}?>
-									</tbody>
-								</table>
-							</div>
+									}?>
+								</tbody>
+							</table>
 						</div>
-						<hr>
+					</div>
+					<hr>
 					<form action="clearance!.php" method="POST">
 						<input type="hidden" name="roll_no" value='<?php echo $roll_no ?>'>
+
 						<div class="row-fluid">
 							<div class="span2">
 								<button type="submit" id="comfirm" class="btn btn-large btn-success"
-								        name="confirm"><i class="icon-ok"></i> Pay&nbsp&nbsp&nbsp
+								        name="confirm"><i class="icon-ok"></i> Confirm
 								</button>
 							</div>
 							<div class="span2">
