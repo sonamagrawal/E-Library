2be1160b2fab7e1e70b3dda4fb2d3d55c835df99
sonam <sonamagrawalnitrr@gmai.com>
2013-07-27 01:27:24 +0530

    functionality to add category

---
+++ resources/category!.php
@@ -0,0 +1,6 @@
+<?php
+include_once("config.php");
+$name = strtoupper(trim($_POST['name']));
+$query = "insert into category (name) values ('" . $name . "')";
+mysql_query($query);
+header("location:add_category.php");
---
+++ resources/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+
+
+    <style>
+
+
+    </style>
+
+
+</head>
+
+<body>
+<div class="container-fluid">
+    <div class="row-fluid">
+        <div class="span2" style="border:1px solid black";>
+            <p>fgjfyjgyg</p>
+        </div>
+
+        <div class="span10" style="border:1px solid black";>
+            <p>cjgfyfyg</p>
+        </div>
+    </div>
+
+
+</div>
+
+
+</body>
+
+
+</html>
