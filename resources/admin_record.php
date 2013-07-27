86fdb3ceda635b5760b1cfb5e7f40c34ef493fda
imsinu9 <imsinu9@gmail.com>
2013-07-28 01:53:43 +0530

    Adding feature to alert user by alert messages when adding admin

--- resources/admin_record.php
+++ resources/admin_record.php
@@ -3,8 +3,18 @@
 	<head>
 		<link href="/public_html/css/bootstrap.css" rel="stylesheet">
 		<link href="/public_html/css/bootstrap-responsive.css" type="text/css" rel="stylesheet">
+		<script src="/public_html/jquery/jquery.js"></script>
+		<script src="/public_html/js/bootstrap-alert.js"></script>
 	</head>
 	<body>
+		<?php
+		if (isset($_GET['m']))
+			if ($_GET['m'] == '1')
+				echo '<div class="alert alert-success">
+			<button type="button" class="close" data-dismiss="alert">&times;</button>
+			<strong>Successfull!</strong> Admin added succesfuly.
+		</div>';
+		?>
 		<div class="container-fluid">
 			<div class="span9 well" style="border:1px solid black">
 				<div class="row-fluid">
