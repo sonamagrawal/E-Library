51279b7d64786d389904cde22c8f29c073fe6b0e
sonam <sonamagrawalnitrr@gmai.com>
2013-07-27 20:46:53 +0530

    nav and login final added backgrond color

--- resources/index.php
+++ resources/index.php
@@ -22,7 +22,8 @@ $category_list = '[' . rtrim($category, ",") . ']';
 	<link href="/public_html/css/bootstrap-responsive.css" type="text/css" rel="stylesheet">
 	<link href="/public_html/css/home-style.css" type="text/css" rel="stylesheet">
 </head>
-<body>
+<body style="background-color: #ffffcc">
+
 <?php
 include_once('header.html') ;
 ?>
