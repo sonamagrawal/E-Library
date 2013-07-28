f198284b166fd0c2824ee38f50c51160e859ade6
imsinu9 <imsinu9@gmail.com>
2013-07-28 06:49:53 +0530

    Setting up an session variable for user name

--- resources/login!.php
+++ resources/login!.php
@@ -3,8 +3,8 @@ include_once("config.php");
 $user_name = strtoupper(trim($_POST['username']));
 $pwd = strtoupper(trim($_POST['password']));
 
-$select_user = "Select user_id,user_type from login where user_name = '" . $user_name . "' and
-				password = md5(" . $pwd . ")";
+$select_user = "Select user_id,user_type,user_name from login where user_name = '" . $user_name . "'
+				and password = md5(" . $pwd . ")";
 $user = mysql_query($select_user);
 
 if (mysql_num_rows($user) > 0) {
@@ -12,13 +12,28 @@ if (mysql_num_rows($user) > 0) {
 
 	session_start();
 	$_SESSION['u_id'] = $row['user_id'];
+	$user_name = $row['user_name'];
 
-	if ($row['user_type'] == 'A')
+	if ($row['user_type'] == 'A') {
+		get_name("admin_record", "employee_no");
 		header("location:admin.php");
-	else
+	} elseif ($row['user_type'] == 'S') {
+		get_name("student_record", "roll_no");
 		header("location:profile.php");
+	} else {
+		get_name("teacher_record", "employee_no");
+		header("location:profile.php");
+	}
 
 } else {
 	header("location:javascript://:history.go(-1)");
+}
+function get_name($table, $column)
+{
+	$query = "select first_name, last_name from " . $table . " where " . $column . "
+			   = '" . $GLOBALS['user_name'] . "'";
+	$res = mysql_query($query);
+	$row = mysql_fetch_array($res);
+	$_SESSION['name'] = ucfirst(strtolower($row['first_name'])) . " " . ucfirst(strtolower($row['last_name']));
 }
 
